5f935b4431d6c2b78dc493385b986eeac8e58e9f
git-imon <git@imonweb.co.uk>
2022-04-14 00:13:48 +0100

    route

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const grandparent = document.getElementById('grandparent-id')
+// const grandparent = document.getElementById('grandparent-id')
 // const parents = document.getElementsByClassName('parent')
-const parents = Array.from(document.getElementsByClassName('parent'))
+const grandparent = document.querySelector(".grandparent")
+const parents = Array.from(grandparent.children)
+// const parents = Array.from(document.getElementsByClassName('parent'))
+
+// const grandparent = document.querySelector(grandparent-id)
 
 // This will throw error because getElementsByClassName will return collection 
 // but collection doesn't have forEach method
 parents.forEach(changeColor)
 
 // changeColor(grandparent)
-changeColor(parents)
+// changeColor(parents)
+
+const childOne = document.querySelector("#childOne")
+const parent = childOne.parentElement
+const grandParent = parent.parentElement
+
+changeColor(childOne)
 
 function changeColor(element){
     element.style.backgroundColor = "#333"
